da8470d227ff39e48a5c39579da92588bdffcbe1
seanbamford <104382216+seanbamford@users.noreply.github.com>
2023-08-23 18:00:05 -0400

    finished extras

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import './App.css'
 
+
 function Game() {
   const [history, setHistory] = useState<Array<Array<string | null>>>([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
   const xIsNext: boolean = currentMove % 2 === 0;
   const currentSquares: Array<string | null> = history[currentMove];
-
+  const [isAscending, setIsAscending] = useState<boolean>(true);
+  interface Coordinates {
+    row: number | undefined;
+    column: number | undefined;
+  }
   function handlePlay(nextSquares: Array<string | null>) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
@@ -19,13 +24,14 @@ function Game() {
 
   const moves = history.map((move: Array<string | null>) => {
     let description: string;
+    const coordinates: Coordinates = getCoordinates(move, history[history.indexOf(move)-1])
+    const location: string = " (Row: " + coordinates.row + " Column: " + coordinates.column + ")";
     const i = history.indexOf(move);
     if (i != 0) {
-      description = "Go to move #" + Number(history.indexOf(move) + 1);
+      description = "Go to move #" + Number(history.indexOf(move) + 1) + location;
     } else {
       description = "Go to game start";
     }
-
     if (history[currentMove] !== move || currentMove == 0) {
       return (
         <li key={i}>
@@ -33,27 +39,45 @@ function Game() {
         </li>
       );
     } else {
-      return <div>{"You are at move #" + (currentMove + 1)}</div>;
+      return <div key={i}>{"You are at move #" + (currentMove + 1)}</div>;
     }
   });
+
+  function handleSort() {
+    setIsAscending(!isAscending)
+  }
+  function getCoordinates(now: Array<string | null>, prev?: Array<string | null>): Coordinates {  
+    let index: number | null;
+    if(prev){
+    index = now.findIndex((element: string|null, idx: number) => element !== prev[idx]);}
+    else {index = now.findIndex((element: string|null) => element && true)}
+    const row = Math.floor(index / 3) +1;
+    const column = index % 3 + 1;
+  
+    return { row, column };
+  }
+  const sortedMoves: JSX.Element[] = isAscending ? [...moves] : [...moves].reverse();
 
   return (
     <div className="game">
       <div className="game-board">
-        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+        <Board xIsNext={xIsNext} squares={currentSquares} moveNumber={currentMove} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-        <ol>{moves}</ol>
+        <h6>Move List</h6>
+        <button onClick={handleSort}>Sort Moves</button>
+        <ol>{sortedMoves}</ol>
       </div>
     </div>
   );
 }
 
-function Square({ value, onSquareClick }: { value: string | null; onSquareClick: () => void }) {
-  return <button className="square" onClick={onSquareClick}>{value}</button>;
+function Square({ value, onSquareClick, isWin }: { value: string | null; onSquareClick: () => void; isWin: boolean; }) {
+  const classNames = "square" + (isWin ? " win" : "");
+  return <button className={classNames} onClick={onSquareClick}>{value}</button>;
 }
 
-function Board({ xIsNext, squares, onPlay }: { xIsNext: boolean; squares: Array<string | null>; onPlay: (nextSquares: Array<string | null>) => void }) {
+function Board({ xIsNext, squares, moveNumber, onPlay }: { xIsNext: boolean; squares: Array<string | null>; moveNumber: number; onPlay: (nextSquares: Array<string | null>) => void }) {
   function handleClick(i: number) {
     if (squares[i] || calculateWinner(squares)) return;
     const nextSquares: Array<string | null> = squares.slice();
@@ -61,17 +85,38 @@ function Board({ xIsNext, squares, onPlay }: { xIsNext: boolean; squares: Array<
     else nextSquares[i] = "O";
     onPlay(nextSquares);
   }
-  // const grid = () => {
-  //   for (let i = 0; i < 9; i++) {
-  //     <Square value={squares[i]} onSquareClick={() => handleClick(i)}
-  //   }
-  //   //return <Square value={squares[i]} onSquareClick={() => handleClick(i)} />
-  // }
+  const lines: Array<Array<number>> = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  const winningSquares: Array<number> = [];
+  function calculateWinner(squares: Array<string | null>): string | null {
+    
+    for (const [a, b, c] of lines) {
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        winningSquares.push(a, b, c);
+        return squares[a];
+      }
+    }
+
+    return null;
+  }
   const winner = calculateWinner(squares);
+  const winSquares = winner ? winningSquares : [];
   let status: string;
+
   if (winner) {
     status = winner + " wins!";
-  } else {
+  } else if (moveNumber > 8) {
+    status = "It's a draw."
+  }
+  else {
     status = (xIsNext ? "X" : "O") + "'s turn.";
   }
 
@@ -82,11 +127,13 @@ function Board({ xIsNext, squares, onPlay }: { xIsNext: boolean; squares: Array<
         <div key={row} className="board-row">
           {[0, 1, 2].map((col) => {
             const index = row * 3 + col;
+            const isWinningSquare = winSquares.includes(index);
             return (
               <Square
                 key={col}
                 value={squares[index]}
                 onSquareClick={() => handleClick(index)}
+                isWin={isWinningSquare}
               />
             );
           })}
@@ -96,25 +143,6 @@ function Board({ xIsNext, squares, onPlay }: { xIsNext: boolean; squares: Array<
   );
 }
 
-function calculateWinner(squares: Array<string | null>): string | null {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
 
-  for (const [a, b, c] of lines) {
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-
-  return null;
-}
 
 export default Game;
