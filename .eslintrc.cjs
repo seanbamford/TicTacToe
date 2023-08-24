ffa3e0089ce5a71eaa33a94973c82b83007b36b6
seanbamford <104382216+seanbamford@users.noreply.github.com>
2023-08-24 12:43:28 -0400

    format

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,98 +1,98 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2022: true,
-  },
-  extends: ['plugin:react/recommended', 'airbnb', 'plugin:@typescript-eslint/recommended', 'plugin:etc/recommended', 'plugin:unicorn/recommended', 'prettier'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    sourceType: 'module',
-    project: './tsconfig.json',
-  },
-  ignorePatterns: ['vite.config.ts'],
-  plugins: ['react', '@typescript-eslint', 'react-hooks', 'unicorn', 'prettier'],
-  rules: {
-    'prettier/prettier': 'error',
-    'no-use-before-define': 'off',
-    '@typescript-eslint/no-use-before-define': ['error'],
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-filename-extension': [
-      'warn',
-      {
-        extensions: ['.tsx', '.js'],
-      },
-    ],
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        ts: 'never',
-        tsx: 'never',
-      },
-    ],
-    'no-shadow': 'off',
-    '@typescript-eslint/no-shadow': ['error'],
-    'max-len': 'off',
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-    'react/function-component-definition': [
-      'error',
-      {
-        namedComponents: 'arrow-function',
-      },
-    ],
-    'import/prefer-default-export': 'warn',
-    'comma-dangle': 'off',
-    'import/no-unresolved': [
-      'error',
-      {
-        ignore: ['^react$'],
-      },
-    ],
-    'import/no-extraneous-dependencies': [
-      'error',
-      {
-        devDependencies: ['src/stories/**/*.stories.tsx'],
-      },
-    ],
-    'no-multiple-empty-lines': 'error',
-    'unicorn/prevent-abbreviations': 'off',
-    'unicorn/filename-case': [
-      'error',
-      {
-        case: 'pascalCase',
-      },
-    ],
-    'no-tabs': 'off',
-    semi: ['error', 'never'],
-    'linebreak-style': ['error', 'windows'],
-    'jsx-quotes': ['error', 'prefer-single'],
-    'prefer-arrow-callback': 'error',
-    indent: 'off',
-    'unicorn/switch-case-braces': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-empty-function': 'off',
-    'jsx-a11y/click-events-have-key-events': 'warn',
-    'jsx-a11y/no-static-element-interactions': 'warn',
-    'jsx-a11y/alt-text': 'warn',
-    radix: 'off',
-    'unicorn/prefer-number-properties': 'off',
-    'sort-keys': 'off',
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': ['error'],
-    'react/require-default-props': 'off',
-    'react/destructuring-assignment': 'off',
-  },
-  settings: {
-    'import/resolver': {
-      typescript: {},
-    },
-    react: {
-      version: '18',
-    },
-  },
+	env: {
+		browser: true,
+		es2022: true,
+	},
+	extends: ['plugin:react/recommended', 'airbnb', 'plugin:@typescript-eslint/recommended', 'plugin:etc/recommended', 'plugin:unicorn/recommended', 'prettier'],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 12,
+		sourceType: 'module',
+		project: './tsconfig.json',
+	},
+	ignorePatterns: ['vite.config.ts'],
+	plugins: ['react', '@typescript-eslint', 'react-hooks', 'unicorn', 'prettier'],
+	rules: {
+		'prettier/prettier': 'error',
+		'no-use-before-define': 'off',
+		'@typescript-eslint/no-use-before-define': ['error'],
+		'react/react-in-jsx-scope': 'off',
+		'react/jsx-filename-extension': [
+			'warn',
+			{
+				extensions: ['.tsx', '.js'],
+			},
+		],
+		'import/extensions': [
+			'error',
+			'ignorePackages',
+			{
+				ts: 'never',
+				tsx: 'never',
+			},
+		],
+		'no-shadow': 'off',
+		'@typescript-eslint/no-shadow': ['error'],
+		'max-len': 'off',
+		'react-hooks/rules-of-hooks': 'error',
+		'react-hooks/exhaustive-deps': 'warn',
+		'react/function-component-definition': [
+			'error',
+			{
+				namedComponents: 'arrow-function',
+			},
+		],
+		'import/prefer-default-export': 'warn',
+		'comma-dangle': 'off',
+		'import/no-unresolved': [
+			'error',
+			{
+				ignore: ['^react$'],
+			},
+		],
+		'import/no-extraneous-dependencies': [
+			'error',
+			{
+				devDependencies: ['src/stories/**/*.stories.tsx'],
+			},
+		],
+		'no-multiple-empty-lines': 'error',
+		'unicorn/prevent-abbreviations': 'off',
+		'unicorn/filename-case': [
+			'error',
+			{
+				case: 'pascalCase',
+			},
+		],
+		'no-tabs': 'off',
+		semi: ['error', 'never'],
+		'linebreak-style': ['error', 'windows'],
+		'jsx-quotes': ['error', 'prefer-single'],
+		'prefer-arrow-callback': 'error',
+		indent: 'off',
+		'unicorn/switch-case-braces': 'off',
+		'@typescript-eslint/explicit-function-return-type': 'off',
+		'@typescript-eslint/no-empty-function': 'off',
+		'jsx-a11y/click-events-have-key-events': 'warn',
+		'jsx-a11y/no-static-element-interactions': 'warn',
+		'jsx-a11y/alt-text': 'warn',
+		radix: 'off',
+		'unicorn/prefer-number-properties': 'off',
+		'sort-keys': 'off',
+		'no-unused-vars': 'off',
+		'@typescript-eslint/no-unused-vars': ['error'],
+		'react/require-default-props': 'off',
+		'react/destructuring-assignment': 'off',
+	},
+	settings: {
+		'import/resolver': {
+			typescript: {},
+		},
+		react: {
+			version: '18',
+		},
+	},
 }
--- src/App.tsx
+++ src/App.tsx
@@ -92,27 +92,28 @@ function Board({
 	squares: Array<string | null>
 	moveNumber: number
 	onPlay: (nextSquares: Array<string | null>) => void
-}) {	const lines: Array<Array<number>> = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-]
-const winningSquares: Array<number> = []
-function calculateWinner(squares: Array<string | null>): string | null {
-  for (const [a, b, c] of lines) {
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      winningSquares.push(a, b, c)
-      return squares[a]
-    }
-  }
+}) {
+	const lines: Array<Array<number>> = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	]
+	const winningSquares: Array<number> = []
+	function calculateWinner(squares: Array<string | null>): string | null {
+		for (const [a, b, c] of lines) {
+			if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+				winningSquares.push(a, b, c)
+				return squares[a]
+			}
+		}
 
-  return null
-}
+		return null
+	}
 	function handleClick(i: number) {
 		if (squares[i] || calculateWinner(squares)) return
 		const nextSquares: Array<string | null> = squares.slice()
